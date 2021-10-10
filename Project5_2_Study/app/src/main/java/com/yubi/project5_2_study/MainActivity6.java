d130cb9e509757d2e7585872ad7cdffdef12a422
Kassy729 <hamtol98@gmail.com>
2021-10-10 21:49:08 +0900

    commit

---
+++ Project5_2_Study/app/src/main/java/com/yubi/project5_2_study/MainActivity6.java
@@ -0,0 +1,14 @@
+package com.yubi.project5_2_study;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main6);
+    }
+}
